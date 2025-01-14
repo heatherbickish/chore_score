346b886388a157bc71c8d74fa369e6680d182b52
heather bickish <hbickish@gmail.com>
2025-01-14 14:19:06 -0700

    inserted data to db

--- dbSetup.sql
+++ dbSetup.sql
@@ -12,8 +12,22 @@ CREATE TABLE IF NOT EXISTS accounts(
 CREATE TABLE chores(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
-  description VARCHAR(255),
-  isComplete BOOLEAN DEFAULT false
+  description VARCHAR(255) NOT NULL,
+  isComplete BOOLEAN DEFAULT false NOT NULL
 );
 
 DROP TABLE chores;
+
+INSERT INTO 
+chores(name, description, isComplete)
+VALUES("Walk the kitties", "Don't go to PetCo, we are banned for life.", 1),
+("Get out of the house", "Go for a walk, run to the store, anything! I'm turning hermity", 0),
+("Drop off donations", "They've been in the back of the car for the last 7 months.", 1),
+("Clean the kitchen", "I have no more clean silverware and 1 bowl", 1),
+("Write letter", "They won't know unless you write it...", 0);
+
+SELECT * FROM chores;
+
+SELECT * FROM chores WHERE id = 2;
+
+DELETE FROM chores WHERE id = 1;
