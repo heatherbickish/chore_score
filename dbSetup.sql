718c1943894b672d09e29e73ad5d7cc9be7a3884
heather bickish <hbickish@gmail.com>
2025-01-14 14:00:02 -0700

    created table, inserted con string

---
+++ dbSetup.sql
@@ -0,0 +1,19 @@
+-- Active: 1736871714737@@127.0.0.1@3306@insightful_ocean_7962_db
+CREATE TABLE IF NOT EXISTS accounts(
+  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  name varchar(255) COMMENT 'User Name',
+  email varchar(255) UNIQUE COMMENT 'User Email',
+  picture varchar(255) COMMENT 'User Picture'
+) default charset utf8mb4 COMMENT '';
+
+
+CREATE TABLE chores(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description VARCHAR(255),
+  isComplete BOOLEAN DEFAULT false
+);
+
+DROP TABLE chores;
